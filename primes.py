20cc9acab9e445ba495cd6c790d9182903e2212f
Nhu Quynh Nguyen <nngu0106@student.monash.edu>
2022-05-16 18:57:03 +1000

    Prime Number

--- primes.py
+++ primes.py
@@ -1,2 +1,21 @@
 def largest_prime(k: int) -> int:
-    raise NotImplementedError()
+
+    factor_list = []
+    divisor = 2
+
+    while k >= 2:
+        flag = False
+        while k % divisor == 0:
+            flag = True
+            k /= divisor
+
+        if flag:
+            factor_list.append(divisor)
+
+    largest_prime_num = factor_list[-1]
+
+    return largest_prime_num
+
+
+
+
