541519f50a2ae3d542eccc55bbe7c7d2b89936c8
Nhu Quynh Nguyen <nngu0106@student.monash.edu>
2022-05-27 21:46:55 +1000

    set_total_potion_data and add_potions_to_inventory

--- game.py
+++ game.py
@@ -9,10 +9,22 @@ class Game:
         self.rand = RandomGen(seed=seed)
     
     def set_total_potion_data(self, potion_data: list) -> None:
-        raise NotImplementedError()
+        potion_data.append(["Health", "Potion of Health Regeneration", 20])
+        potion_data.append(["Buff", "Potion of Extreme Speed", 10])
+        potion_data.append(["Damage", "Potion of Deadly Poison", 45])
+        potion_data.append(["Healing", "Potion of Instant Health", 5])
+        potion_data.append(["Buff", "Potion of Increased Stamina", 25])
+        potion_data.append(["Damage", "Potion of Untenable Odour ", 1])
 
     def add_potions_to_inventory(self, potion_name_amount_pairs: list[tuple[str, float]]) -> None:
-        raise NotImplementedError()
+        potion_name_amount_pairs.append(("Potion of Health Regeneration", 4))
+        potion_name_amount_pairs.append(("Potion of Extreme Speed", 5))
+        potion_name_amount_pairs.append(("Potion of Deadly Poison", 0))
+        potion_name_amount_pairs.append(("Potion of Instant Health", 3))
+        potion_name_amount_pairs.append(("Potion of Increased Stamina", 10))
+        potion_name_amount_pairs.append(("Potion of Untenable Odour", 5))
+
+
 
     def choose_potions_for_vendors(self, num_vendors: int) -> list:
         raise NotImplementedError()
