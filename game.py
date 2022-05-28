9b7a9825478b822a389e72fbbcbf7532f7dc9df0
BEHNAM MOZAFARI <bmoz0002@student.monash.edu>
2022-05-28 13:39:50 +1000

    finished choose_potions_for_vendors, add_potions_to_inventory, set_total_potion_data

--- game.py
+++ game.py
@@ -10,23 +10,58 @@ class Game:
 
     def __init__(self, seed=0) -> None:
         self.rand = RandomGen(seed=seed)
-        self.total_potion = None
-        self.inventory = []
+        self.total_potions = None
+        self.inventory = None
 
     def set_total_potion_data(self, potion_data: list) -> None:
-        self.total_potion = LinearProbePotionTable(len(potion_data), True)
+        """
+        Creates hash table to store all possible potions, sets inventory of vendors to contain 0 of each potion
+        Args:
+            potion_data: List containing all possible potions
+        :complexity: O(N*log(N)), where N is the length of potion_data
+        """
+        self.total_potions = LinearProbePotionTable(len(potion_data))
+        self.inventory = AVLTree()
         for i in potion_data:
             new_potion = Potion.create_empty(i[1], i[0], i[2])
-            self.total_potion.insert(new_potion.name, (new_potion.potion_type, new_potion.buy_price))
+            self.total_potions.insert(new_potion.name, (new_potion.potion_type, new_potion.buy_price))
+            self.inventory[new_potion.buy_price] = (new_potion.name, 0)
 
     def add_potions_to_inventory(self, potion_name_amount_pairs: list[tuple[str, float]]) -> None:
-        self.inventory = AVLTree()
+        """
+        Adds litres of particular potions into the inventory of vendors
+        Args:
+            potion_name_amount_pairs: list of tuples containing two elements: The name of the potion, and amount in litres to add
+             to the inventory.
+        :complexity: O(C*log(N)), where C is the length of potion_name_amount_pairs, and N is the number of potions in
+        self.total_potions
+        """
         for i in potion_name_amount_pairs:
-            # Not sure what to put instead of the self.inventory.root
-            self.inventory.insert_aux(self.inventory.root, i[0], (i[1], i[2]))
+            price = self.total_potions[i[0]][2]
+            self.inventory[price] = (i[0], i[1])
 
     def choose_potions_for_vendors(self, num_vendors: int) -> list:
-        raise NotImplementedError()
+        """
+        Completes the vendor potion selection process
+        Args:
+            num_vendors: number of vendors who will sell potions
+        :complexity: O(C*log(N)), where C is equal to num_vendors, and N is the number of potions in
+        self.total_potions
+        """
+        output = []
+        prices = []
+        for i in range(num_vendors):
+            potion_stock = len(self.inventory)
+            p = self.rand.randint(potion_stock)
+            potion = self.inventory.kth_largest(p)
+            output.append(potion.item)
+            prices.append(potion.key)
+            del self.inventory[potion.key]
+
+        for j in range(num_vendors):
+            self.inventory[prices[j]] = output[j]
+
+        return output
 
     def solve_game(self, potion_valuations: list[tuple[str, float]], starting_money: list[int]) -> list[float]:
         raise NotImplementedError()
